dbc229be26c92df8dd35ec84c04f15e317569372
jeast00 <38165949+jeast00@users.noreply.github.com>
2021-03-20 14:41:44 -0400

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+
+
+function map(num, callBackFunction) {
+    let n = [];
+
+    for(let i = 0; i < num.length; i++) {
+        let value = num[i];
+        n.push(callBackFunction(value));
+    }
+    return n;
+}
+
+function reduce(num, callBackFunction, startPoint) {
+
+    let n = (!!startPoint) ? startPoint : num[0];
+    let i = (!!startPoint) ? 0 : 1;
+    for(; i < num.length; i++) {
+        n = callBackFunction(num[i], n);
+    }
+
+    return n;
+}
